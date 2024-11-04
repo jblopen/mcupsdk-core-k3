6c67a2e74d54f229eb22ea15ea7c032fff0b3cef
Edouard Haas <edouard.haas@jblopen.com>
2024-11-04 10:12:47 -0500

    am62ax: filex: add missing nor/nand driver source files.
    - add missing nor/nand driver source files.
    
    Fixes: SITSW-5614
    Signed-off-by: Edouard Haas <edouard.haas@jblopen.com>

---
+++ source/fs/filex/filex_levelx/lx_nand_flash_driver.h
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (C) 2018-2024 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef LX_NAND_FLASH_DRIVER_H
+#define LX_NAND_FLASH_DRIVER_H
+
+#include <stdint.h>
+#include <lx_api.h>
+
+
+UINT  lx_nand_driver_create(LX_NAND_FLASH *p_nand_flash, uint32_t flash_instance_id, uint32_t offset,
+                            uint32_t size, const UINT spare_map[][2], size_t spare_map_length);
+
+UINT lx_nand_driver_init(LX_NAND_FLASH *p_nand_flash);
+
+uint64_t lx_nand_driver_size_get(LX_NAND_FLASH *p_nand_flash);
+
+
+#endif
