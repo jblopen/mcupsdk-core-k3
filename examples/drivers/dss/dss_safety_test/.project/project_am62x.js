2fcb47c540033990e2603495b1318d845878d841
Soumya Tripathy <s-tripathy@ti.com>
2024-11-28 15:50:47 +0530

    am62x: DSS: Add support for dss safety test example.
    - This commit enables DSS safety test example for am62x soc on
    am62x-sk, am62x-sk-lp, and am62x-sip-sk board.
    - All the a53ss0-0 dss_safety_test example mmu configuration
    reserves entry for DSS frame buffer region.
    - Default DSS display test example in enabled with OLDI configuration
    (SK-LCD1)
    
    Fixes: SITSW-6023
    
    Signed-off-by: Soumya Tripathy <s-tripathy@ti.com>

---
+++ examples/drivers/dss/dss_safety_test/.project/project_am62x.js
@@ -0,0 +1,118 @@
+let path = require('path');
+
+let device = "am62x";
+
+const files = {
+    common: [
+        "dss_safety_test.c",
+        "dss_display_pattern.c",
+        "main.c",
+    ],
+};
+
+/* Relative to where the makefile will be generated
+ * Typically at <example_folder>/<BOARD>/<core_os_combo>/<compiler>
+ */
+const filedirs = {
+    common: [
+        "..",       /* core_os_combo base */
+        "../../..", /* Example base */
+        "../../../../dss_test_pattern"
+    ],
+};
+
+const libdirs_freertos_a53 = {
+	common: [
+		"${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+		"${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+		"${MCU_PLUS_SDK_PATH}/source/board/lib",
+    ],
+};
+
+const includes_freertos_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/GCC/ARM_CA53",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am62x/a53",
+    ],
+};
+
+const libs_freertos_a53 = {
+    common: [
+        "freertos.am62x.a53.gcc-aarch64.${ConfigName}.lib",
+        "drivers.am62x.a53.gcc-aarch64.${ConfigName}.lib",
+        "board.am62x.a53.gcc-aarch64.${ConfigName}.lib",
+    ],
+};
+
+const lnkfiles = {
+    common: [
+        "linker.cmd",
+    ]
+};
+
+const syscfgfile = "../example.syscfg"
+
+const readmeDoxygenPageTag = "EXAMPLES_DRIVERS_DSS_SAFETY_TEST";
+
+const templates_freertos_a53 =
+[
+    {
+        input: ".project/templates/am62x/common/linker_a53.cmd.xdt",
+        output: "linker.cmd",
+        options: {
+            dssFrameBuf: "true",
+        },
+    },
+    {
+        input: ".project/templates/am62x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "dss_safety_test_main",
+        },
+    }
+];
+
+const buildOptionCombos = [
+    { device: device, cpu: "a53ss0-0", cgt: "gcc-aarch64",  board: "am62x-sk", os: "freertos"},
+    { device: device, cpu: "a53ss0-0", cgt: "gcc-aarch64",  board: "am62x-sip-sk", os: "freertos"},
+    { device: device, cpu: "a53ss0-0", cgt: "gcc-aarch64",  board: "am62x-sk-lp", os: "freertos"},
+];
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "executable";
+    property.name = "dss_safety_test";
+    property.isInternal = false;
+    property.isLinuxFwGen = true;
+    property.buildOptionCombos = buildOptionCombos;
+
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.files = files;
+    build_property.filedirs = filedirs;
+    build_property.lnkfiles = lnkfiles;
+    build_property.syscfgfile = syscfgfile;
+    build_property.readmeDoxygenPageTag = readmeDoxygenPageTag;
+
+    if(buildOption.cpu.match(/a53*/)){
+        build_property.includes = includes_freertos_a53;
+        build_property.libdirs = libdirs_freertos_a53;
+        build_property.libs = libs_freertos_a53;
+        build_property.templates = templates_freertos_a53;
+
+    }
+
+    return build_property;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+};
